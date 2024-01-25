28b1868eb97d03d3d52e12c55a6d28eab908f437
beatriz amarilla <betty.182.186@gmail.com>
2024-01-25 20:15:15 -0300

    add cards characters printing

--- app.js
+++ app.js
@@ -6,17 +6,6 @@ const hash = "&hash=31c8321bf2c8dca37913226c7e83701d";
 const $ = (selector) => document.querySelector(selector)
 let title = ""
 
-//data de personajes 
-const getMarvelCharacters = async() =>{
-    const url = `${urlBase}characters?${ts}${publicKey}${hash}`
-    const response = await fetch(url)
-    const data = await response.json()
-    console.log(data.data.results)
-    return data.data.results
-}
-
-getMarvelCharacters()
-
 //data de comics
 const getMarvelComics = async(title) =>{
     let existTitle= title?`&titleStartsWith=${title}`:""
@@ -34,9 +23,9 @@ const getMarvelComics = async(title) =>{
 const printComics = async(tittle)=>{
     const comics = await getMarvelComics(tittle)
     //console.log(comics)
-    $(".characters-cards").innerHTML = ``
+    $(".comics-cards").innerHTML = ``
     for(let comic of comics){
-        $(".characters-cards").innerHTML += `
+        $(".comics-cards").innerHTML += `
         <div>
         <img src="${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}" alt="${comic.title}">
         <p>${comic.title}</p>
@@ -47,7 +36,7 @@ const printComics = async(tittle)=>{
 }
 printComics()
 
-//<p>${comic.thumbnail}<p>
+
 //input text 
 
 
@@ -56,9 +45,47 @@ printComics()
             printComics($(".input-text").value)
         }) 
     }) 
-    
+  
+// personajes    
+let name = ""
 
-    
+ //data de personajes 
+const getMarvelCharacters = async(name) =>{
+    let existName= name?`&nameStartsWith=${name}`:""
+    const url = `${urlBase}characters?${ts}${publicKey}${hash}${existName}`
+    const response = await fetch(url)
+    const data = await response.json()
+console.log(data.data.results)
+    return data.data.results
+}
+
+
+//impresion de personajes en pantalla
+
+const printCharacters = async(name)=>{
+    const characters = await getMarvelCharacters(name)
+    $(".characters-cards").innerHTML = ``
+    for(let character of characters){
+        $(".characters-cards").innerHTML += `
+        <div>
+        <img src="${character.thumbnail.path}/portrait_xlarge.${character.thumbnail.extension}" alt="${character.name}">
+        <p>${character.name}</p>
+        </div>
+
+        `
+    }
+}
+printCharacters ()
+
+
+ //input text 
+
+    $(".input-text").addEventListener("input", ()=>{
+        $(".button-search").addEventListener("click", ()=>{
+            printCharacters($(".input-text").value)
+        }) 
+    }) 
+     
 
 
 
