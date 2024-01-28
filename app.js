39559c9a0f862839523ac3e21469ca8c32c44672
beatriz amarilla <betty.182.186@gmail.com>
2024-01-28 19:41:54 -0300

    fix select

--- app.js
+++ app.js
@@ -44,15 +44,6 @@ const printComics = async(tittle)=>{
 }
 printComics()
 
-
-//input text 
-
-
-    $(".input-text").addEventListener("input", ()=>{
-        $(".button-search").addEventListener("click", ()=>{
-            printComics($(".input-text").value)
-        }) 
-    }) 
   
 // personajes    
 let name = ""
@@ -91,32 +82,88 @@ const printCharacters = async(name)=>{
 printCharacters ()
 
 
-     
-
-
 // Al cargar la página
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".comics-cards").classList.remove("ocultoComics");
     document.querySelector(".characters-cards").classList.add("ocultoCharacters");
 });
 
-document.querySelector(".button-search").addEventListener("click", () => {
 
-    const selectedValue = document.querySelector("select").value;
+// Obtener referencia a los elementos del DOM
+const buttonSearch = document.querySelector(".button-search");
+const selectElement = document.querySelector("select");
+const selectOrden = document.getElementById("selectOrden");
+const comicsCards = document.querySelector(".comics-cards");
+const charactersCards = document.querySelector(".characters-cards");
+const inputText = document.querySelector(".input-text");
+const cardCharactersOnly = document.getElementById("characters-cards-only");
+
+
+
+
+// Agregar un listener al botón de búsqueda
+buttonSearch.addEventListener("click", () => {
+    const selectedValue = selectElement.value;
 
     if (selectedValue === "COMICS") {
-        document.querySelector(".comics-cards").classList.remove("ocultoComics");
-        document.querySelector(".characters-cards").classList.add("ocultoCharacters");
-        printComics();
+        comicsCards.classList.remove("ocultoComics");
+        charactersCards.classList.add("ocultoCharacters");
+        cardCharactersOnly.style.display = "none";
+        printComics(inputText.value);
 
     } else if (selectedValue === "PERSONAJES") {
-        document.querySelector(".comics-cards").classList.add("ocultoComics");
-        document.querySelector(".characters-cards").classList.remove("ocultoCharacters");
-        printCharacters();
+        comicsCards.classList.add("ocultoComics");
+        charactersCards.classList.remove("ocultoCharacters");
+        cardCharactersOnly.style.display = "none";
+        selectOrden.querySelector('option[value="NUEVOS"]').style.display = 'none';
+        selectOrden.querySelector('option[value="VIEJOS"]').style.display = 'none';
+        printCharacters(inputText.value);
+    }
+});
+
+// Evitar el envío del formulario por defecto al hacer clic en el botón
+buttonSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+});
+
+
+
+selectElement.addEventListener('change', (e) => {
+    if (e.target.value === 'PERSONAJES') {
+        selectOrden.querySelector('option[value="NUEVOS"]').style.display = 'none';
+        selectOrden.querySelector('option[value="VIEJOS"]').style.display = 'none';
+    } else {
+        selectOrden.querySelectorAll('option').forEach(option => option.style.display = 'block');
     }
 });
 
 
+
+
+
+
+
+
+
+
+
+
+//comics
+// let title1 = ""
+
+// //data de comics
+// const getMarvelComics1 = async(title) =>{
+//     let existTitle= title?`&titleStartsWith=${title}`:""
+//     const url = `${urlBase}comics?${ts}${publicKey}${hash}${existTitle}${limit}`
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data.data)
+//     return { 
+//         comics:data.data.results.id,
+       
+//     }
+// }
+// console.log(getMarvelComics1())
 
 
 
